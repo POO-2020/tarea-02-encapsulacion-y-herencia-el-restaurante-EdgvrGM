da5552190a0653da62ad08da9ce623998eab7ffe
Edgar Gonzalez <edgar_gm6@hotmail.com>
2020-03-19 12:26:48 -0600

    encapsulado y funcionando

---
+++ precio.js
@@ -0,0 +1,24 @@
+const formatterDolar = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  })
+
+export default class Precio{
+
+    
+
+    constructor(valor)
+    {
+        this._valor = valor     
+    }
+
+    
+    getPrecio(){
+        
+        return (`${formatterDolar.format(this._valor)}`)
+    }
+
+    getValor(){
+        return this._valor
+    }
+}
---
+++ restaurante.js
@@ -0,0 +1,63 @@
+export default class Restaurante{
+
+    constructor(nombre, telefono, direccion)
+    {
+        this._nombre = nombre
+        this._telefono = telefono
+        this._direccion = direccion
+        this._productos = []
+        this._pedidos = []
+    }
+
+    registrarProducto(producto){
+        this._productos.push(producto)
+    }
+    registrarPedido(pedido){
+       
+       let found = this.buscarPedido(pedido)
+      
+        if(found == null){this._pedidos.push(pedido)
+            return true}
+        else
+        {return false}
+
+        
+    }
+
+    listarProductos(){
+        console.log("~ P R O D U C T O S ~")
+        this._productos.forEach( (pro) => {
+            console.log(pro.getDescripcion())
+        })
+    }
+    listarPedidos(){
+        console.log("~ P E D I D O S ~")
+        this._pedidos.forEach( (ped, i) => {
+            console.log(`(${i+1}) ~ ${ped.getResumen()}`)
+        })
+    }
+    
+    buscarPedido(pedido){
+        let buscar = this._pedidos.find(ped => ped._esIgualA(pedido))
+        if(buscar == undefined){return null}
+        else {return buscar}
+    }
+    eliminarPedido(pedido){
+        let found =  this.buscarPedido(pedido)
+        if(found == null) return false
+        else {
+        let i = this._pedidos.indexOf(found)
+        this._pedidos.splice(i,1)
+        return true
+        }
+    }
+    modificarPedido(pedido, nuevoPedido){
+        let found =  this.buscarPedido(pedido)
+        if(found == null) return false
+        else {
+        let i = this._pedidos.indexOf(found)
+        this._pedidos[i] = nuevoPedido
+        return true
+        }
+    }
+}
